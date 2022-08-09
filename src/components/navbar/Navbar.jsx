1bc1f59e4940f8872856c282ba49ab28066bc5f8
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-08-10 02:29:42 +0530

    fixed som pixels

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,18 +11,18 @@ function Navbar() {
           <img src={logo} alt="" className="img-responsive" />
         </div>
         <div className="flex flex-center navigations">
-          <div className="nav-item">
-            <h5>Demo Custom App</h5>
+          <div className="nav-item ">
+            <h4>Demo Custom App</h4>
             <p>APP NAME</p>
           </div>
           <MdNavigateNext />
-          <div className="nav-item">
-            <h5>Assessment</h5>
+          <div className="nav-item ">
+            <h4>Assessment</h4>
             <p>STAGE</p>
           </div>
           <MdNavigateNext />
-          <div className="nav-item">
-            <h5>Create PO</h5>
+          <div className="nav-item ">
+            <h4>Create PO</h4>
             <p>BUTTON</p>
           </div>
           <MdNavigateNext />
@@ -30,7 +30,7 @@ function Navbar() {
         </div>
       </div>
       <div className="flex flex-center">
-        <h4>App saved on 27 jul 2017 4:23am</h4>
+        <p className="time">App saved on 27 jul 2017 4:23am</p>
         <button className="primary-btn">
             DONE
         </button>
--- src/components/rule/AddRule.jsx
+++ src/components/rule/AddRule.jsx
@@ -5,18 +5,18 @@ function AddRule() {
     return (
         <div className='sidebar'>
            <header className='flex flex-align-center'>
-            <MdNavigateBefore/>
-            <span>Back to Stages</span>
+            <MdNavigateBefore className='bold'/>
+            <p className='bold'>Back to Stages</p>
            </header>
             <div className="rules-cont">
                 <p>RULES 2</p>
                 <div className="rules flex">
-                    <input type="text" className='input'/>
-                    <MdContentCopy className='copy-icon'/>
+                    <div>Create PO</div>
                     <MdOutlineDeleteForever className='delete-icon'/>
                 </div>
                 <div className='fallback'>
                     <h3>Fall back rule</h3>
+                    
                 </div>
                 <button className='primary-btn'>
                     Add New Rule
